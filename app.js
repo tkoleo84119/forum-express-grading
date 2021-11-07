7a9d1487c9116fcd9ae2d57820dc30d62fd1d7b1
tkoleo84119 <tkoleo84119@gmail.com>
2021-11-07 23:56:06 +0800

    feat: add flash-message & confirm user password and unique email

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
-const db = require('./models')
+const flash = require('connect-flash')
+const session = require('express-session')
 const app = express()
 const port = 3000
 
@@ -9,6 +10,14 @@ app.engine('hbs', handlebars({ defaultLayout: 'main', extname: '.hbs' })) // Han
 app.set('view engine', 'hbs')
 
 app.use(express.urlencoded({ extended: true }))
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
+app.use(flash())
+
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
