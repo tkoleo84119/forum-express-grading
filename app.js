2100a8b362af713b7904ba0eb2e286af6040a4ac
tkoleo84119 <tkoleo84119@gmail.com>
2021-11-07 21:51:21 +0800

    feat: add signup page

--- app.js
+++ app.js
@@ -4,8 +4,11 @@ const db = require('./models')
 const app = express()
 const port = 3000
 
+// 套件相關settings
 app.engine('hbs', handlebars({ defaultLayout: 'main', extname: '.hbs' })) // Handlebars 註冊樣板引擎
 app.set('view engine', 'hbs')
+
+app.use(express.urlencoded({ extended: true }))
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 // 載入所需套件
 const restController = require('../controllers/restController.js')
 const adminController = require('../controllers/adminController.js')
+const userController = require('../controllers/userController.js')
+
 
 module.exports = (app) => {
   // restaurants routes setting
@@ -10,4 +12,8 @@ module.exports = (app) => {
   // admin routes setting
   app.get('/admin', (req, res) => res.redirect('/admin/restaurants'))
   app.get('/admin/restaurants', adminController.getRestaurants)
+
+  // signup routes setting
+  app.get('/signup', userController.signUpPage)
+  app.post('/signup', userController.signUp)
 }
