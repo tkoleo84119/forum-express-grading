3934d71a86e1947cd6303a1a7f10e14946c46c9f
tkoleo84119 <tkoleo84119@gmail.com>
2021-11-21 22:13:32 +0800

    refactor: modify routes(PUT /api/admin/restaurants/:id)

--- services/adminService.js
+++ services/adminService.js
@@ -60,6 +60,38 @@ const adminService = {
     }
   },
 
+  putRestaurant: async (req, res, callback) => {
+    try {
+      if (!req.body.name) {
+        callback({ status: 'error', message: "name didn't exist" })
+      }
+
+      const { file } = req
+      if (file) {
+        imgur.setClientID(IMGUR_CLIENT_ID)
+        imgur.upload(file.path, async (err, img) => {
+          const restaurant = await Restaurant.findByPk(req.params.id)
+          await restaurant.update({
+            ...req.body,
+            image: file ? img.data.link : restaurant.image,
+            CategoryId: req.body.categoryId
+          })
+          callback({ status: 'success', message: 'restaurant was successfully updated' })
+        })
+      } else {
+        const restaurant = await Restaurant.findByPk(req.params.id)
+        await restaurant.update({
+          ...req.body,
+          image: restaurant.image,
+          CategoryId: req.body.categoryId
+        })
+        callback({ status: 'success', message: 'restaurant was successfully updated' })
+      }
+    } catch (err) {
+      console.log(err)
+    }
+  },
+
   deleteRestaurant: async (req, res, callback) => {
     try {
       await Restaurant.destroy({ where: { id: req.params.id } })
